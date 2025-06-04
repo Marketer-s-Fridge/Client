7ff1efff01ded020413c845f7eaee3eb0b119185
zzizi <zzi_zi@naver.com>
2025-06-05 00:18:14 +0900

    fix: 모바일화면 제목 폰트 크기 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,13 +19,13 @@ export default function Header() {
     <header className="border-b border-gray-300 px-[5%] lg:px-[17%] py-6 sm:py-3 flex flex-col md:flex-row md:justify-between md:items-center gap-y-5 md:gap-y-0 ">
       <Link
         href="/"
-        className="text-red-500 font-bold text-5xl md:text-sm font-playfair self-center md:self-baseline"
+        className="text-red-500 font-bold text-4xl md:text-sm font-playfair self-center md:self-baseline"
       >
         Marketer&apos;s Fridge
       </Link>
 
       {/* 네비게이션 */}
-      <nav className="flex flex-wrap justify-center md:justify-end md:gap-x-10 gap-x-[5%] gap-y-2 text-xs text-gray-700">
+      <nav className="flex flex-wrap justify-center md:justify-end md:gap-x-10 gap-x-[4%] gap-y-2 text-xs text-gray-700">
         {navItems.map((item) => {
           const isActive = pathname === item.href;
           const isLoginJoin = item.name === "로그인 | 회원가입";
