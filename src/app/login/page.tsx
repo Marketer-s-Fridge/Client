f90b129e68d3fd9410e488579e4468a319b7a1bd
zzizi <zzi_zi@naver.com>
2025-06-12 20:15:16 +0900

    chore: 회원가입, 아이디찾기, 비밀번호 찾기 cursor-pointer 설정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -116,13 +116,24 @@ const LoginPage: React.FC = () => {
           <SubmitButton text="로그인" onClick={() => setShowConfirm(true)} />
 
           <div className="flex justify-center gap-4 text-[11px] text-[#757575] mb-10 mt-3">
-            <button onClick={() => router.push("/signUp")}>회원가입</button>
+            <button
+              onClick={() => router.push("/signUp")}
+              className="cursor-pointer"
+            >
+              회원가입
+            </button>
             <span>|</span>
-            <button onClick={() => router.push("/login/findId")}>
+            <button
+              onClick={() => router.push("/login/findId")}
+              className="cursor-pointer"
+            >
               아이디 찾기
             </button>
             <span>|</span>
-            <button onClick={() => router.push("/login/findPwd")}>
+            <button
+              onClick={() => router.push("/login/findPwd")}
+              className="cursor-pointer"
+            >
               비밀번호 찾기
             </button>
           </div>
