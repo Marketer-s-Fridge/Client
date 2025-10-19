a08473d182ef5209744366855f7602056242a9d2
zzizi <zzi_zi@naver.com>
2025-10-19 10:39:05 +0900

    chore: next.config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  /* config options here */
+  output: "standalone",
+
   eslint: {
     ignoreDuringBuilds: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // 클라이언트에서 호출할 경로
+        destination: "http://mf-api:8081/api/:path*", // ✅ Docker 내부의 Spring Boot 컨테이너로 전달
+      },
+    ];
   },
 };
 
