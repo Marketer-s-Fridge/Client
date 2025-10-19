398dc25cae3653a9d4d02c0a1cf6067abe43e3a9
zzizi <zzi_zi@naver.com>
2025-10-19 13:10:02 +0900

    chore: api 포트 수정

--- next.config.ts
+++ next.config.ts
@@ -12,12 +12,12 @@ const nextConfig: NextConfig = {
       // ✅ auth 요청은 /api 없이 바로 백엔드로
       {
         source: "/auth/:path*",
-        destination: "http://mf-api:8081/auth/:path*",
+        destination: "http://mf-api:8080/auth/:path*",
       },
       // ✅ 나머지는 /api 그대로 백엔드로 전달
       {
         source: "/api/:path*",
-        destination: "http://mf-api:8081/api/:path*",
+        destination: "http://mf-api:8080/api/:path*",
       },
     ];
   },
