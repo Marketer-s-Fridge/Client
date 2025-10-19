fd7956058a10924cfc10ea9998cf23cadd017045
zzizi <zzi_zi@naver.com>
2025-10-19 18:13:34 +0900

    chore: api 주소 수정

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,25 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "standalone",
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
 
   async rewrites() {
     return [
       // ✅ auth 요청은 /api 없이 바로 백엔드로
       {
         source: "/auth/:path*",
-        destination: "http://mf-api:8080/auth/:path*",
+        destination:
+          process.env.NODE_ENV === "production"
+            ? "http://mf-api:8080/auth/:path*"
+            : "http://localhost:8081/auth/:path*",
       },
-      // ✅ 나머지는 /api 그대로 백엔드로 전달
+      // ✅ 나머지 /api 요청
       {
         source: "/api/:path*",
-        destination: "http://mf-api:8080/api/:path*",
+        destination:
+          process.env.NODE_ENV === "production"
+            ? "http://mf-api:8080/api/:path*"
+            : "http://localhost:8081/api/:path*",
       },
     ];
   },
