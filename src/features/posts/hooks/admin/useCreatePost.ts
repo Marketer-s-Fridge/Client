1dff85c5e35600d2e7d7cf7f378fee105c8676c2
zzizi <zzi_zi@naver.com>
2025-11-09 16:12:45 +0900

    api: post hook 오류 수정

--- src/features/posts/hooks/admin/useCreatePost.ts
+++ src/features/posts/hooks/admin/useCreatePost.ts
@@ -1,4 +1,4 @@
-// useCreatePost.ts
+// src/features/posts/hooks/admin/useCreatePost.ts
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createPost } from "../../api/postsApi";
 import { PostRequestDto, PostResponseDto } from "../../types";
@@ -7,7 +7,7 @@ export function useCreatePost() {
   const queryClient = useQueryClient();
 
   return useMutation<PostResponseDto, Error, PostRequestDto>({
-    mutationFn: createPost,
+    mutationFn: (dto) => createPost(dto),   // ← 래핑해서 1인자만 전달
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["posts"] });
     },
