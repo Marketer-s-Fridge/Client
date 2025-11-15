b8fd1962231edebd6eefbe60e000aa05096aa2d3
zzizi <zzi_zi@naver.com>
2025-11-15 15:23:15 +0900

    refactor: next.config.ts 이미지 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@
 const nextConfig = {
   output: "standalone",
   eslint: { ignoreDuringBuilds: true },
+
+  // ✅ 여기에 이미지 도메인 추가
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mfridge-images.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
