50b217866bf330b5e00bf2b0bbe3fbd90a78a306
zzizi <zzi_zi@naver.com>
2025-12-09 17:11:39 +0900

    fix: env 수정

--- src/utils/getKakaoAuthUrl.ts
+++ src/utils/getKakaoAuthUrl.ts
@@ -1,21 +1,19 @@
 export const getKakaoAuthUrl = () => {
-    const clientId = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
-    const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+  const clientId = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
+  const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
 
-    if (!clientId || !redirectUri) {
-      console.error("Kakao env not set");
-      return "#";
-    }
-    console.log("KAKAO KEY", clientId);
+  if (!clientId || !redirectUri) {
+    console.error("Kakao env not set");
+    return "#";
+  }
 
-  
-    const params = new URLSearchParams({
-      client_id: clientId,
-      redirect_uri: redirectUri,
-      response_type: "code",
-      scope: "account_email"
-    });
-  
-    return `https://kauth.kakao.com/oauth/authorize?${params.toString()}`;
-  };
-  
+  const params = new URLSearchParams({
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    response_type: "code",
+    scope: "account_email",
+    prompt: "login" // 선택: 사파리/아이폰에서 재인증 꼬임 방지
+  });
+
+  return `https://kauth.kakao.com/oauth/authorize?${params.toString()}`;
+};
